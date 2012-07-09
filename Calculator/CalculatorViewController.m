245918d0df0f00c28799d1ea8ece4f62849bc0d4
Shitian Long <longst.cn@gmail.com>
2012-07-09 13:26:39 +0200

    fixed feature in assignment 1

--- Calculator/CalculatorViewController.m
+++ Calculator/CalculatorViewController.m
@@ -7,37 +7,12 @@
 //
 
 #import "CalculatorViewController.h"
-#import "CalculatorLogic.h"
-
-typedef enum{
-    OPER_PLUS=1,
-    OPER_MINUS,
-    OPER_MUTIPLY,
-    OPER_DIVIDE,
-    OPER_LOG,
-    OPER_SQIT,
-    OPER_COS,
-    OPER_SIN,
-    OPER_PI,
-    OPER_E,
-    OPER_OPT_SIGN,
-}CAL_OPERATOR;
-
-typedef enum{
-    FUNC_BACKWARD=1,
-    FUNC_CLEAR,
-    FUNC_ENTER,
-}CAL_FUNC;
-
-typedef enum{
-    DIGIT_DOT = 10
-}CAL_DIGIT;
-
+#import "CalculatorBrain.h"
 
 @interface CalculatorViewController ()
 
 @property (nonatomic) BOOL userIsInTheMiddleOfEnteringANumber;
-@property (nonatomic, strong) CalculatorLogic *calLogic;
+@property (nonatomic, strong) CalculatorBrain *calLogic;
 
 - (void)displayHandlerWithoutEnter:(NSString *)operator;
 - (void)cleanDisplay;
@@ -46,14 +21,14 @@ typedef enum{
 @implementation CalculatorViewController
 
 @synthesize display = _display;
-@synthesize log = _log;
+@synthesize inputDisplay = _log;
 @synthesize userIsInTheMiddleOfEnteringANumber = _userIsInTheMiddleOfEnteringANumber;
 @synthesize calLogic = _calLogic;
  
 
-- (CalculatorLogic *)calLogic{
+- (CalculatorBrain *)calLogic{
     if (!_calLogic) {
-        _calLogic = [[CalculatorLogic alloc] init];
+        _calLogic = [[CalculatorBrain alloc] init];
     }
     return _calLogic;
 }
@@ -62,13 +37,12 @@ typedef enum{
 {
     [super viewDidLoad];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(cleanDisplay) name:@"RPN_ERROR" object:nil];
-	// Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)viewDidUnload
 {
     [self setDisplay:nil];
-    [self setLog:nil];
+    [self setInputDisplay:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
@@ -85,28 +59,17 @@ typedef enum{
                                           otherButtonTitles:nil];
     [error show];
     self.display.text = [NSString stringWithString:@""];
-    self.log.text = [NSString stringWithString:@""];
+    self.inputDisplay.text = [NSString stringWithString:@""];
 }
 
 
 //digit pressed
 //tag property is defined as corresponding number
 - (IBAction)digitPressed:(UIButton *)sender {
-    
     NSString *digitPressed = [NSString stringWithFormat:@"%d", sender.tag];
     
     // Handle Dot
     if (sender.tag == DIGIT_DOT && [self.display.text rangeOfString:@"."].location == NSNotFound){
-        self.userIsInTheMiddleOfEnteringANumber = YES;
-        if (self.display.text.length == 0) {
-            self.display.text = @"0.";
-        }
-        else {
-            self.display.text = [NSString stringWithFormat:@"%@%@"
-                                 ,self.display.text
-                                 ,@"."
-                                 ];
-        }
         
     }
     
@@ -124,152 +87,42 @@ typedef enum{
 
 }
 
-
-
-// entre is not pressed, during user is entering a number
-// Entre logic should be here
-- (void)displayHandlerWithoutEnter:(NSString *) operator{
-    
-    // for two operands operation
-    if ([operator isEqualToString:@"+"] 
-        || [operator isEqualToString:@"-"] 
-        || [operator isEqualToString:@"*"] 
-        || [operator isEqualToString:@"/"]){
-        
-        if (self.userIsInTheMiddleOfEnteringANumber) {
-            if (self.log.text.length == 0) {
-                self.log.text = [NSString stringWithFormat:@"%@ %@ %@"
-                                 , @"0"
-                                 , operator
-                                 , self.display.text];
-            }
-            else {
-                self.log.text = [NSString stringWithFormat:@"%@ %@ %@"
-                                 , self.log.text
-                                 , operator
-                                 , self.display.text];
-            }
-            
-            self.userIsInTheMiddleOfEnteringANumber = NO;
-            //Handle logic
-            [self.calLogic pushInStack:self.display.text];
-        }
-        else {
-            if (self.log.text.length == 0) {
-                // Nothing happens when there is no operand in display
-                return;
-            }
-            else {
-                self.log.text = [NSString stringWithFormat:@"%@ %@"
-                                 , self.log.text
-                                 , operator
-                                 ];
-            }
-        }
-        
+- (IBAction)dicimalPressd {
+    // dicimal in front of number
+    if (!self.userIsInTheMiddleOfEnteringANumber) {
+        self.display.text = @"0.";
+        self.userIsInTheMiddleOfEnteringANumber = YES;
     }
     
-    // for single operand operation
-    else {
-        if (self.userIsInTheMiddleOfEnteringANumber) {
-            self.log.text = [NSString stringWithFormat:@"%@ %@"
-                             , operator
-                             , self.display.text
-                             ];
-            [self.calLogic pushInStack:self.display.text];
-        }
-        else {
-            self.log.text = [NSString stringWithFormat:@"%@ %@"
-                             , operator
-                             , self.display.text
-                             ];
-        }
+    // no dicimal found in current string
+    else if([self.display.text rangeOfString:@"."].location == NSNotFound){
+        self.display.text = [self.display.text stringByAppendingString:@"."];
     }
-    
 }
+
 
 
 - (IBAction)operationPressed:(UIButton *)sender {    
     
-    //Handle Display
-    switch (sender.tag) {
-        case OPER_COS:{
-            [self displayHandlerWithoutEnter: @"COS"];
-            self.display.text = [self.calLogic performOperationCos];
-            break;
-        }
-            
-        case OPER_DIVIDE:{
-            [self displayHandlerWithoutEnter: @"/"];
-            self.display.text = [self.calLogic performOperationDivide];
-            break;
-        }
-            
-        case OPER_E:{
-             [self displayHandlerWithoutEnter: @"e"];
-            self.display.text = [self.calLogic performOperationE];
-            break;
-        }
-            
-        case OPER_LOG:{
-            [self displayHandlerWithoutEnter: @"log"];
-            self.display.text = [self.calLogic performOperationLog];
-            break;
-        }
-            
-        case OPER_MINUS:{
-            [self displayHandlerWithoutEnter: @"-"];
-            self.display.text = [self.calLogic performOperationMinus];
-            break;
-        }
-            
-        case OPER_MUTIPLY:{
-            [self displayHandlerWithoutEnter: @"*"];
-            self.display.text = [self.calLogic performOperationMutiply];
-            break;
-        }
-            
-        case OPER_OPT_SIGN:{
-            [self displayHandlerWithoutEnter: @"(+/-)"];
-            self.display.text = [self.calLogic performOperationOptSign];
-            break;
-        }
-            
-        case OPER_PI:{
-            self.display.text = [self.calLogic performOperationPi];
-            break;
-        }
-            
-        case OPER_PLUS:{
-            [self displayHandlerWithoutEnter: @"+"];
-            self.display.text = [self.calLogic performOperationPlus];
-            break;
-        }
-            
-        case OPER_SIN:{
-            [self displayHandlerWithoutEnter: @"SIN"];
-            self.display.text = [self.calLogic performOperationSin];
-            break;
-        }
-            
-        case OPER_SQIT:{
-            self.display.text = [self.calLogic performOperationSqit];
-            break;
-        }
-            
-        default:
-            break;
+    if (self.userIsInTheMiddleOfEnteringANumber) {
+        [self enterPressed];
     }
+    double result = [self.calLogic performOperation:sender.tag];
     
-    //protect error case like 12/0
+    self.display.text = [NSString stringWithFormat:@"%g", result];
+    
+    self.inputDisplay.text = [NSString stringWithFormat:@"%@ %@"
+                              , self.inputDisplay.text
+                              , [self.calLogic getOperator:sender.tag]
+                              ];
     // Handle Log
     if (self.display.text.length == 0) {
-        self.log.text = [NSString stringWithString:@""];
+        self.inputDisplay.text = [NSString stringWithString:@""];
         return;
     }
     else {
-        self.log.text = [NSString stringWithFormat:@"%@ %@ %@"
-                         ,self.log.text
+        self.inputDisplay.text = [NSString stringWithFormat:@"%@ %@ %@"
+                         ,self.inputDisplay.text
                          ,@"="
                          ,self.display.text];
     }    
@@ -277,12 +130,31 @@ typedef enum{
 }
 
 
+- (IBAction)enterPressed{
+    self.userIsInTheMiddleOfEnteringANumber = NO;
+    // if display end with ".", "." should be removed  
+    if ([self.display.text hasSuffix:@"."]) {
+        self.display.text = [self.display.text substringToIndex:([self.display.text length] - 1)];
+    }
+    
+    //Handle UI
+    self.inputDisplay.text = [NSString stringWithFormat:@"%@ %@ "
+                              ,self.inputDisplay.text
+                              ,self.display.text];
+    
+    //Handle logic
+    [self.calLogic pushOperand:[self.display.text doubleValue]];
+}
+
+
 - (IBAction)funcPressed:(UIButton *)sender {
+    
+    self.userIsInTheMiddleOfEnteringANumber = NO;
     
     switch (sender.tag) {
         case FUNC_BACKWARD:{
             
-            [self.calLogic pushInStack:self.display.text];
+            [self.calLogic pushOperand:[self.display.text doubleValue]];
             //Handle UI
             if ([self.display.text length] > 0){
                 self.display.text = [self.display.text substringToIndex:[self.display.text length] - 1];
@@ -296,24 +168,12 @@ typedef enum{
         case FUNC_CLEAR:{
             //Handl UI
             self.display.text = [NSString stringWithString:@""];
-            self.log.text = [NSString stringWithString:@""];
+            self.inputDisplay.text = [NSString stringWithString:@""];
             
             //Handle logic
-            [self.calLogic clearOperand];
+            [self.calLogic clearBrain];
             break;
-        }
-            
-        case FUNC_ENTER:{
-            //Handle UI
-            self.log.text = [NSString stringWithFormat:@"%@ %@ "
-                             ,self.log.text
-                             ,self.display.text];
-            self.userIsInTheMiddleOfEnteringANumber = NO;
-            //Handle logic
-            [self.calLogic pushInStack:self.display.text];
-            break;
-        }
-                        
+        }          
         default:
             break;
     }
@@ -321,9 +181,5 @@ typedef enum{
 }
 
 
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
-    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
-}
 
 @end
